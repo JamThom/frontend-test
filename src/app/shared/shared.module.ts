7f23853178d11b780308a84a1e12c5036648e411
Jamie Thomson <jamthomweb@gmail.com>
2019-12-31 11:28:16 +0100

    title bar styling

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TitleBarComponent } from '@shared/components/title-bar/title-bar.component';
 import { SidebarNavigationComponent } from './components/sidebar-navigation/sidebar-navigation.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 
 
 @NgModule({
-  declarations: [TitleBarComponent, SidebarNavigationComponent],
+  declarations: [TitleBarComponent, SidebarNavigationComponent, LogoComponent],
   imports: [
     CommonModule
   ],
