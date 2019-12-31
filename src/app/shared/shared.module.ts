6acc9cd040c2fd6b3a0680df3ee5819a3cf691c8
Jamie Thomson <jamthomweb@gmail.com>
2019-12-31 12:00:25 +0100

    title bar profile styling

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TitleBarComponent } from '@shared/components/title-bar/title-bar.component';
+import { TitleBarComponent } from './components/title-bar/title-bar.component';
 import { SidebarNavigationComponent } from './components/sidebar-navigation/sidebar-navigation.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { TitleBarProfileComponent } from './components/title-bar-profile/title-bar-profile.component';
