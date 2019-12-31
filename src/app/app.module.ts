61ff4ffa67da78afd0d22f278593c93596096ec1
Jamie Thomson <jamthomweb@gmail.com>
2019-12-31 10:41:36 +0100

    folder structure init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
+import { TitleBarComponent } from '@shared/components/title-bar/title-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, TitleBarComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
