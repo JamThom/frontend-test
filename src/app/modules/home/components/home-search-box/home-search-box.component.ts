62090f7b803be188184871bdf3d2daf548e59ad4
Jamie Thomson <jamthomweb@gmail.com>
2020-01-02 17:10:49 +0100

    search box component input binding

--- src/app/modules/home/components/home-search-box/home-search-box.component.ts
+++ src/app/modules/home/components/home-search-box/home-search-box.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-home-search-box',
   templateUrl: './home-search-box.component.html',
   styleUrls: ['./home-search-box.component.scss']
 })
-export class HomeSearchBoxComponent implements OnInit {
+export class HomeSearchBoxComponent {
 
-  constructor() { }
+  @Output() search = new EventEmitter();
 
-  ngOnInit() {
+  searchTerm = '';
+
+  handleSearch(event: InputEvent) {
+    this.search.emit((event.target as HTMLInputElement).value);
   }
 
 }
